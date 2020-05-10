3028ae5ff3f504143bb1b5b8c0dfcdf93e154b96
Luis Parada <luis.parada@a3sec.com>
2020-05-10 18:02:15 +0200

    master settings

--- deck_pocket_backend/deck_pocket_backend/settings.py
+++ deck_pocket_backend/deck_pocket_backend/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import firebase_admin
 import logging
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,17 +83,8 @@ WSGI_APPLICATION = 'deck_pocket_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DeckPocket',
-        'USER': 'postgres',
-        'PASSWORD': 'docker',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-}
+DATABASES = {}
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -208,3 +202,6 @@ LOGGING = {
         },
     },
 }
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
